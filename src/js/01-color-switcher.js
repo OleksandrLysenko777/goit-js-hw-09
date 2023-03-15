12a6a73a32281ec4feb8fecabf9004486298dbbb
OleksandrLysenko777 <114435880+OleksandrLysenko777@users.noreply.github.com>
2023-03-15 15:02:58 +0100

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const changeColor = 1000;
+let interval = null;
 
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+refs.btnStart.addEventListener('click', onBtnStartChange);
+refs.btnStop.addEventListener('click', onBtnStopChange);
+refs.btnStop.disable = true;
+function onBtnStartChange() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  interval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, changeColor);
+}
+function onBtnStopChange() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(interval);
+}
